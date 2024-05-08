4a77685b653ba0885c643626c78a829089474e03
Chara-X <1719552189@qq.com>
2024-05-09 03:02:20 +0800

    Update StreamingCall.go

--- StreamingCall.go
+++ StreamingCall.go
@@ -5,13 +5,15 @@ import (
 	"os"
 )
 
-func StreamingCall(cli *http.Client, url string, header map[string][]string, body any) (req *http.Request, reqBody *Encoder, res *http.Response, resBody *Decoder) {
+func StreamingCall(cli *http.Client, url string, header http.Header, body any) (req *http.Request, reqBody *Encoder, res *http.Response, resBody *Decoder) {
 	var r, w, _ = os.Pipe()
-	NewEncoder(w).Encode(body)
+	if reqBody = NewEncoder(w); body != nil {
+		reqBody.Encode(body)
+	}
 	req, _ = http.NewRequest(http.MethodPost, url, r)
 	req.Header = header
 	req.Header.Add("Content-Type", "application/grpc+json")
 	req.Header.Add("Te", "trailers")
 	res, _ = cli.Do(req)
-	return req, NewEncoder(w), res, NewDecoder(res.Body)
+	return req, reqBody, res, NewDecoder(res.Body)
 }
